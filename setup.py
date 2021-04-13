4eba56f83b153a7ad704ffcafe9e630b53f21ec4
John Parton <john.parton.iv@gmail.com>
2021-04-13 13:56:40 -0500

    Use setuptools-git-versioning

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup(
+        # I can't seem to figure out how to move this to setup.cfg
         version_config={
             "starting_version": "0.0.3"
         },
